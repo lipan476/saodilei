01e5e83ac69de7923d56d4eda5bec7a5aa9e6850
lipan <670339228@qq.com>
2025-04-08 17:50:49 +0800

    提交代码

---
+++ src/settings.c4418.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"1f6c5",internal:"52d36",main:"08091"}};
