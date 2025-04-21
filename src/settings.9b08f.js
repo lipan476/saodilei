9c141508f17defc19766d7d0e92c0c2ac68f5baa
lipan <670339228@qq.com>
2025-04-21 13:26:39 +0800

    修复代码

--- src/settings.9b08f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/script/game/ui/scene/EveryScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"80013",main:"65ed7"}};
